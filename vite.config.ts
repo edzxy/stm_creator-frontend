33c20c20505d629a462461846780ca450db30f36
Edward Zhang <u7873639@anu.edu.au>
2025-10-01 00:42:12 +1000

    fix allow host issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true
+  },
+  preview: {
+    allowedHosts: true
+  },
   build: {
     sourcemap: true // optional for dev, required for prod debugging
   },
